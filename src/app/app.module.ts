157d44bdcf87511782bb3ed61175cf986201cc27
Sámán <31960469+player1s@users.noreply.github.com>
2018-08-05 13:22:05 +0200

    firebase basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireModule} from 'angularfire2';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FIREBASE_CONFIG } from './firebase.credentials';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAuC9bZLWywlWWto_8taPdWzDBiWrpu-6s",
+    authDomain: "checklistextended.firebaseapp.com",
+    databaseURL: "https://checklistextended.firebaseio.com",
+    projectId: "checklistextended",
+    storageBucket: "",
+    messagingSenderId: "720758269701"
+};
